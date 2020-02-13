0e0ff4c30994c28b0c0b43b65fd36a2b9e1ad75a
Cristian Pintado <cpintado17@gmail.com>
2020-02-13 02:05:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["Hi", "bae", "Te"]
+  array = ["wow", "I", "am", "really", "learning"]
   array << "arrays!"
   array
 
