7131024b63d779f9bc61121fa2a67fe1f0a791f1
Cristian Pintado <cpintado17@gmail.com>
2020-02-13 02:51:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,10 +23,9 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.pop
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.pop('arrays!')
-  remove_element_from_start_of_array.pop
+  array
   
 end
 
